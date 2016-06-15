b3899da7361e4b57e2389a905b0cbc909d15f3ed
Nick <fletchernick2@gmail.com>
2016-06-15 00:56:44 -0400

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-	date_default_timezone_set('America/New_York');
-	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = '';
-	$dbname = 'tutorial_01';
-
-	$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die(mysqli_error($connect));
-?>
